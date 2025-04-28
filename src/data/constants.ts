0fc86a0a7cfc7c79f7fa1b4f1f12144c0c6fad92
Abhinav Negi <70994781+senpaii1@users.noreply.github.com>
2025-04-28 12:58:24 +0530

    resume update

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/senpaii1",
   resume:
-    "https://drive.google.com/file/d/1sG7baSg7pe7QgL9ZeeQjHl1lStnQRPc_/view?usp=sharing",
+    "https://drive.google.com/file/d/1rSrm57LN_eZzGurPkSu5ka1n3e4IGaE1/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhinav-negi-6075ab141/",
   twitter: "",
   insta: "https://www.instagram.com/senpaii__1/",
